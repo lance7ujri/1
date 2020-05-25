6780f100e8a9f1daddeb7ad34d36fca05ae0077f
allinext7pol <allinext7pol@gmx.com>
2020-05-25 15:48:05 +0300

    1

---
+++ script/script.js
@@ -0,0 +1,35 @@
+jQuery(document) .ready(function ($){
+
+	$("#lightgallery") .lightGallery({
+		
+	});
+	
+	$('#hash').lightGallery();
+	$('#lightgallery01').lightGallery({
+		galleryId: 2
+	});
+
+	$('#fight_gym').lightGallery({
+		galleryId: 3
+	});
+
+	$('#solarium').lightGallery({
+		galleryId: 4
+	});
+
+	$('#fitness_bar').lightGallery({
+		galleryId: 5
+	});
+
+	$('#fitness_gym').lightGallery({
+		galleryId: 6
+	});
+
+	$('#sauna').lightGallery({
+		galleryId: 7
+	});
+
+	$('#fitness_bar01').lightGallery({
+		galleryId: 8
+	});
+});
